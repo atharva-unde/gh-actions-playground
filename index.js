363c9d9129f866098790a52e1593a290c792f714
Atharva Unde <atharva@confiancebizsol.in>
2025-08-25 12:02:58 +0530

    GHA Testing

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
+});
